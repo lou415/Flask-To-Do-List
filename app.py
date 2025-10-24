a64533ede3f3ac4e5e9f64986c104eb21ed5514a
Luis Ortega <luis.frnando42@gmail.com>
2025-10-24 13:32:20 -0400

    Move db.create_all() outside main block for production

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ def update(id):
     else:
         return render_template('update.html', task = task_to_update)
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug = True)
+    app.run(debug=True)
